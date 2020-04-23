356f9d5f07e4efb87cfa07e6b6cb94f26d254507
Ken Takahashi <takahashi.ken.716@gmail.com>
2020-04-23 07:24:54 +0700

    create NEXT-COOKIE-AUTH

---
+++ components/LoginForm.js
@@ -0,0 +1,67 @@
+import { loginUser } from "./../lib/auth";
+import Router from "next/router";
+
+class LogninForm extends React.Component {
+  state = {
+    email: "Sincere@april.biz",
+    password: "hildegard.org",
+    error: "",
+    isLoading: false,
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    const { email, password } = this.state;
+
+    event.preventDefault();
+    this.setState({ error: "", isLoading: true });
+    loginUser(email, password)
+      .then(() => {
+        Router.push("/profile");
+      })
+      .catch(this.showError);
+  };
+  showError = (err) => {
+    console.error(err);
+    const error = (err.response && err.response.data) || err.message;
+    this.setState({ error, isLoading: false });
+  };
+
+  render() {
+    const { email, password, error, isLoading } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={email}
+            onChange={this.handleChange}
+          ></input>
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            valur={password}
+            onChange={this.handleChange}
+          ></input>
+        </div>
+        <div>
+          <button disabled={isLoading} type="submit">
+            {isLoading ? "Sending" : "Submit"}
+          </button>
+        </div>
+        {error && <div>{error}</div>}
+      </form>
+    );
+  }
+}
+
+export default LogninForm;
---
+++ pages/profile.js
@@ -0,0 +1,23 @@
+import { getUserProfile } from "./../lib/auth";
+import Layout from "./../components/Layout";
+import { authInitialProps } from "./../lib/auth";
+
+export default class Profile extends React.Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    getUserProfile().then((user) => this.setState({ user }));
+  }
+
+  render() {
+    return (
+      <Layout title="Profile" {...this.props}>
+        <pre>{JSON.stringify(this.state.user, null, 2)}</pre>
+      </Layout>
+    );
+  }
+}
+
+Profile.getInitialProps = authInitialProps(true);
